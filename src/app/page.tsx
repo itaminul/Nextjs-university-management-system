137b7075d570c0836038556ce732939b69373c50
itaminul <itaminul@gmail.com>
2023-10-20 23:15:11 +0600

    project setup modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+import AdminDashboard from "./layouts/dashboard/dashboard";
+import Header from "./layouts/header/header";
 import Login from "./layouts/login/page";
+import SideMenu from "./layouts/sideMenu/SideMenu";
 export default function Home() {
   return (
     <>
-      <main>    
+      <main>
+
+
+        
         <Login />
+        {/* <Link href="/layouts/dashboard">Dahsbord</Link> */}
+        {/* <Header /> */}
+        {/* <SideMenu /> */}
+        {/* <AdminDashboard /> */}
       </main>
     </>
   );
