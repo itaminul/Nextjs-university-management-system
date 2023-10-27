116302605e6a890a9d7a90a0ef509694654b297f
itaminul <itaminul@gmail.com>
2023-10-28 05:50:55 +0600

    layout modification

--- src/app/layouts/adminlayout/AdminLayout.tsx
+++ src/app/layouts/adminlayout/AdminLayout.tsx
@@ -1,17 +1,15 @@
 'use client'
-import { Breadcrumb, Layout, Menu } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import Link from 'next/link';
-import { ReactNode } from 'react';
+import { Breadcrumb, Layout } from 'antd';
+import type { ReactNode } from 'react';
 import SideMenu from '../sideMenu/SideMenu';
 
 const { Sider, Content } = Layout;
-
+ 
  type LayoutProps = {
   children: ReactNode,
 }
 
-const AdminLayout = ({ children, ...props }: LayoutProps) => {
+const AdminLayout = ({ children }: LayoutProps) => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider width={180} theme="light">
