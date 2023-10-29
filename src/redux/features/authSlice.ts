6bfacf94d31b482c1512eedb3f03b6e482a0e790
itaminul <itaminul@gmail.com>
2023-10-29 23:31:13 +0600

    login and log out using redux toolkit

---
+++ src/redux/features/authSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type InitialState = {
+  value: AuthState;
+}
+
+type AuthState = {
+  isAuth: boolean,
+  username: string,
+  uid: string,
+  isModerator: boolean,
+
+} 
+
+
+const initialState = {
+  value: {
+    isAuth: false,
+    username: "",
+    uid: "",
+    isModerator: false,
+  },
+} as unknown as InitialState;
+
+export const auth = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logOut: () => {
+      return initialState;
+    },
+    logIn: (state, action: PayloadAction<string>) => {
+      return {
+        value: {
+          isAuth: true,
+          username: action.payload,
+          uid: "afdafadfasfga",
+          isModerator: false,
+        }
+      }
+    },
+    toggleModerator: (state) => {
+      state.value.isModerator = !state.value.isModerator
+    }
+    
+  }
+})
+
+export const { logIn, logOut, toggleModerator } = auth.actions;
+export default auth.reducer;
