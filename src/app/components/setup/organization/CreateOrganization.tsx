8aadc12a13c5246ad82601fdc34eb804171ff530
Aminul PHP <aminul@atilimited.net>
2023-11-04 18:14:29 +0600

    org form modify

--- src/app/components/setup/organization/CreateOrganization.tsx
+++ src/app/components/setup/organization/CreateOrganization.tsx
@@ -1,35 +1,50 @@
 'use client'
-import { Button, Form, Input, Modal, Space } from "antd"
+import { Button, Form, Input, Modal } from "antd"
 import { OrganizationsProps } from "./OrganizationDataType"
+import { Departments } from "../department/CreateDepartmentType";
 
 function CreateOrganization({ title, visible, onCancel}:OrganizationsProps) {
+  const [form] = Form.useForm();
+
+  const handleOk = () => {
+    form.submit()
+}
+
+ const onFinish = (values: Departments) => {
+  const accessToken = localStorage.getItem('accessToken');
+  console.log("value", accessToken);
+
+ }
+
   return(
     <>
       <Modal
       title={title}
       open={visible}
       onCancel={onCancel}
+      footer={
+        [
+          <Button onClick={onCancel}>Close</Button>,
+          <Button form="myForm" key="submit" htmlType="submit" onClick={handleOk}>Submit</Button>
+        ]
+      }
       >
         <Form
         layout="vertical"
         autoComplete="off"
+        form={form} 
+        onFinish={onFinish}
         >
-          <Form.Item name="serialNo" label="Serial No" rules={[{ required: true }]}>
+        <Form.Item name="serialNo" label="Serial No">
             <Input />
           </Form.Item>
         <Form.Item name="orgName" label="Organization Name" rules={[{ required: true }]}>
             <Input />
           </Form.Item>
-          <Form.Item name="orgDescription" label="Organization Description" rules={[{ required: true }]}>
+        <Form.Item name="orgDescription" label="Organization Description" rules={[{ required: true }]}>
             <Input />
           </Form.Item>
-          <Form.Item>
-            <Space>        
-            <div style={{ float: 'right'}}>      
-              <Button htmlType="submit" style={{alignContent:'right'}}>Submit</Button>
-              </div>
-            </Space>
-          </Form.Item>
+          
           </Form>
       </Modal>
     </>
