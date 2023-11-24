2e181c8fa6a0efba2bbf824204211be3ce7e237c
itaminul <itaminul@gmail.com>
2023-11-24 21:49:42 +0600

    create employee form

--- src/app/components/employee/CreateEmployeeForm.tsx
+++ src/app/components/employee/CreateEmployeeForm.tsx
@@ -1,5 +1,18 @@
-import { Col, Divider, Row, Collapse } from 'antd';
+import { Button, Checkbox, Form, Input, Collapse } from 'antd';
 
+const onFinish = (values: any) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo);
+};
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 
 const { Panel } = Collapse
 function CreateEmployeeForm() {
@@ -17,10 +30,50 @@ function CreateEmployeeForm() {
           {/* Add more Collapse Panels as needed */}
         </Collapse>
         <Collapse>
-          {/* Parent form fields go here */}
-          <input type="text" placeholder="Parent Field 1" />
-          <input type="text" placeholder="Parent Field 2" />
-          {/* Add more fields for parent data */}
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: 'Please input your username!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </Collapse>
 
         <Collapse>
