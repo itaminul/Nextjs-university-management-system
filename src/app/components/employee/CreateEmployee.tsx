68ce316246063d0aadc58acdf7a85544a086e1c0
itaminul <itaminul@gmail.com>
2023-11-24 21:59:12 +0600

    create employee list

--- src/app/components/employee/CreateEmployee.tsx
+++ src/app/components/employee/CreateEmployee.tsx
@@ -1,18 +1,9 @@
 'use client'
 import { SettingOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
-import type { CollapseProps } from 'antd';
-import { Collapse, Modal, Select } from 'antd';
+import { Collapse, Modal } from 'antd';
 import { CreateEmployeeProps } from './EmployeeType';
 import CreateEmployeeForm from './CreateEmployeeForm';
-
-const { Option } = Select;
-
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
 
 type ExpandIconPosition = 'start' | 'end';
 
@@ -32,26 +23,6 @@ function CreateEmployee ({visible, title, onCancel}:CreateEmployeeProps) {
     />
   );
 
-  const items: CollapseProps['items'] = [
-    {
-      key: '1',
-      label: 'This is panel header 1',
-      children: <div>{text}</div>,
-      extra: genExtra(),
-    },
-    {
-      key: '2',
-      label: 'This is panel header 2',
-      children: <div>{text}</div>,
-      extra: genExtra(),
-    },
-    {
-      key: '3',
-      label: 'This is panel header 3',
-      children: <div>{text}</div>,
-      extra: genExtra(),
-    },
-  ];
 
   return (
     <>
