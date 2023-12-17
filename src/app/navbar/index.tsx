8e3a14e1a32515b57c1448821ac5962ab1047156
Aminul PHP <aminul@atilimited.net>
2023-12-17 19:45:54 +0600

    employe edit education

--- src/app/navbar/index.tsx
+++ src/app/navbar/index.tsx
@@ -35,6 +35,7 @@ const Navbar = () => {
           <MenuUnfoldOutlined />
         </Button>
       ) : (
+        
         <div className="top-bar">
           <Link href={'/layouts/dashboard'}>           
             <Button type="primary" onClick={showDashbordDrawer}>
